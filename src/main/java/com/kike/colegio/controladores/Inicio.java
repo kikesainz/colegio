8cbb6a8f744dcc22e0155c60af3678011e5727e7
Enrique Saínz-Terrones Peña <6258@CALASANZSALAMANCA.local>
2020-11-12 10:04:22 +0100

    [ADD] Conexión a la BBDD colegio

---
+++ src/main/java/com/kike/colegio/controladores/Inicio.java
@@ -0,0 +1,62 @@
+package com.kike.colegio.controladores;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Inicio
+ */
+
+@WebServlet("/inicio")
+public class Inicio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Inicio() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			String dbURL = "jdbc:mysql://localhost:3306/colegio";
+			String username = "root";
+			String password = "1234";
+			Connection connection = DriverManager.getConnection(dbURL, username, password);
+
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
