06fc50dacda87210baff80f420d0bfbc28c6b47b
Enrique Saínz-Terrones Peña <6258@CALASANZSALAMANCA.local>
2020-11-16 12:23:58 +0100

    [ADD] DAO búsqueda alumno id y nombre

--- src/main/java/com/kike/colegio/dao/AlumnoDAO.java
+++ src/main/java/com/kike/colegio/dao/AlumnoDAO.java
@@ -7,5 +7,6 @@ import com.kike.colegio.dtos.Alumno;
 public interface AlumnoDAO {
 	
 	List<Alumno> obtenerTodosAlumnos();
+	List<Alumno> obtenerAlumnosporIdyNombre(String id, String nombre);
 
 }
--- src/main/java/com/kike/colegio/dao/impl/AlumnoDAOImpl.java
+++ src/main/java/com/kike/colegio/dao/impl/AlumnoDAOImpl.java
@@ -1,6 +1,7 @@
 package com.kike.colegio.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -33,6 +34,33 @@ public class AlumnoDAOImpl implements AlumnoDAO {
 			e.printStackTrace();
 		}		
 
+		return listaAlumnos;
+	}
+
+	@Override
+	public List<Alumno> obtenerAlumnosporIdyNombre(String id, String nombre) {
+		String sql = "SELECT * FROM alumnos WHERE id LIKE ? AND nombre LIKE ?";
+		
+		ResultSet alumnoResultSet = null;
+		Connection connection = DBUtils.DBConnection();
+		List<Alumno> listaAlumnos = new ArrayList<>();
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			
+			ps.setString(1, "%" + id + "%");
+			ps.setString(2, "%" + nombre + "%");
+			
+			alumnoResultSet = ps.executeQuery();	
+			
+			while(alumnoResultSet.next()) {
+				Alumno a = new Alumno (alumnoResultSet.getInt(1), alumnoResultSet.getString(2));
+				listaAlumnos.add(a);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return listaAlumnos;
 	}
 
